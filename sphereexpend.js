d41ff9dc94fe2c18e0d1691e743989a5b219e6fe
Qurka <61182297+Qurka@users.noreply.github.com>
2020-05-22 19:37:59 +0200

    Initial commit

---
+++ sphereexpend.js
@@ -0,0 +1,15 @@
+AFRAME.registerComponent("sphereexpand", {
+  init: function () {
+    let homeworldelements = document.querySelectorAll(".homeworld");
+    let sky = document.querySelector("#sky");
+
+    let sphereloader = () => {
+      sky.setAttribute("src", "#neoworld");
+      homeworldelements.forEach((homeworldelement) => {
+        homeworldelement.setAttribute("visible", false);
+      });
+    };
+
+    this.el.addEventListener("click", sphereloader);
+  },
+});
